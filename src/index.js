f94f8b7ad3bc8fa52c3322f5c8b9f53ea82ea9c9
nikoalteri <nikoalteri@outlook.it>
2023-01-03 17:02:11 +0100

    Backup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 import "./index.scss";
 
