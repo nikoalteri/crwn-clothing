e3f000a57a039db7f12b849c85832213757e4305
nikoalteri <nikoalteri@outlook.it>
2023-01-04 15:27:35 +0100

    Backup

--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -5,26 +5,43 @@ import { CartContext } from "../../contexts/cart.context";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext);
+  const { cartItems, addItemToCart, removeItemToCart } =
+    useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am the checkout page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+
+      {cartItems.map((cartItem) => {
+        const { id, name, quantity } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <br />
+            <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
+            <br />
+            <span onClick={() => addItemToCart(cartItem)}>increment</span>
+          </div>
+        );
+      })}
+      <span className="Total">Total</span>
     </div>
   );
 };
