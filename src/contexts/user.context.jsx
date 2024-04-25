ec8ea873d43dcf91887edf2f36d6261259b022dc
nikoalteri <nikoalteri@outlook.it>
2024-04-25 15:16:00 +0200

    added user context

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
+
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+UserProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
