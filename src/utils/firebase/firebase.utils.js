4e7c01d0919e6507c8049c4ea86d5b906e3edd6e
nikoalteri <nikoalteri@outlook.it>
2024-04-25 16:06:18 +0200

    optimized user context

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,3 +1,4 @@
+import { on } from 'events';
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
@@ -7,6 +8,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -79,3 +81,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => {
   return await signOut(auth);
 };
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
